bb2261856d5e5c2c02ab1434f5ab397575bdffa4
RamitD <rdihidar1997@gmail.com>
2022-05-30 16:02:12 +0530

    initial commit

---
+++ src/Survey.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class Survey extends Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div>Some Survey to help you!!!</div>
+        );
+    }
+}
+
+export default Survey;
